a80bda6ae65c93ffadc5f58682fc81ccc3304e99
Daniel Kusuma <wwwdanielkusuma@gmail.com>
2023-05-29 12:48:47 +0700

    fix bug in package.json

--- app.js
+++ app.js
@@ -2,27 +2,22 @@ const express = require('express');
 const tf = require('@tensorflow/tfjs-node');
 const multer = require('multer');
 const app = express();
-const port = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 
 const upload = multer();
 
-//const modelPath = 'https://storage.googleapis.com/bucket-for-ml-model/model.json';
-
 app.post('/testingModel', upload.single('image'), async (req, res)=>{
     try {
-        const model = await tf.loadLayersModel("https://storage.googleapis.com/bucket-for-ml-model/model.json");
+        const model = await tf.loadLayersModel(process.env.Link_Bucket);
 
          const imageBuffer = req.file.buffer;
-         console.log(imageBuffer);
          const image = tf.node.decodeImage(imageBuffer);
-         console.log(image);
          const processedImage= preprocessImage(image);
          const predictions = await model.predict(processedImage).array();
-
          res.json({predictions});
     } catch (error) {
         console.log(error);
-    res.status(500).json({ error: 'An error occurred' });
+        res.status(500).json({ error: 'An error occurred' });
         
     }
     console.log('predictions should be working');
@@ -30,21 +25,16 @@ app.post('/testingModel', upload.single('image'), async (req, res)=>{
 
 function preprocessImage(image) {
     let rgbImage;    
-  // Resize the image to a fixed size (e.g., 224x224)
+
   const resizedImage = tf.image.resizeBilinear(image, [ 150, 150 ]);
-  // Normalize the pixel values to the range of [0, 1]
-  //console.log(resizedImage.shape[2]);
+
   if(resizedImage.shape[2] !== 3){
      rgbImage = tf.image.grayscaleToRGB(resizedImage);
     }else{
          rgbImage = resizedImage;
     }
   const normalizedImage = rgbImage.div(255.0);
-  //console.log(normalizedImage);
-  // Add a batch dimension to the image
   const batchedImage = normalizedImage.expandDims(0);
-  //console.log(batchedImage);
-
   
   return batchedImage;
 }
@@ -55,6 +45,6 @@ app.get('/', (req,res) =>{
 })
 
 
-app.listen(port, () =>{
-    console.log(`App is working and listenign to port ${port}`);
+app.listen(PORT, () =>{
+    console.log(`App is working and listenign to port ${PORT}`);
 })
